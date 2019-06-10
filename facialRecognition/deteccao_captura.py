e8fc7e82fb1459019a0c62292d1dd1879a7dcdef
Bruno Fornazari <bruno_fornazari@hotmail.com>
2019-06-09 21:44:52 -0300

    debugging

--- facialRecognition/deteccao_captura.py
+++ facialRecognition/deteccao_captura.py
@@ -1,6 +1,6 @@
 import os
-import picamera
-import picamera.array
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import time
 import dlib
 import cv2
@@ -18,45 +18,46 @@ def getUserFromCamera() :
     indices = np.load("resources/indices_captura.pickle", allow_pickle=True)
     descritoresFaciais = np.load("resources/descritores_captura.npy", allow_pickle=True)
     limiar = 0.5
-    cam = cv2.VideoCapture(0)
+    camera = PiCamera()
+    camera.resolution(640, 480)
+    camera.framerate = 32
+    rawCapture = PiRGBArray(camera, size=(640, 480))
     userId = 0
 
     logger.log('Detectando usuário...')
 
-    while userId == 0 :
-        with picamera.PiCamera() as camera:
-            cap = picamera.array.PiRGBArray(camera)
-            camera.resolution = (640, 480)
-            camera.start_preview()
-            time.sleep(0.1)
-            camera.capture(cap, format="bgr")
-            imagem = cap.array
+
+
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        if(userId == 0):
+            imagem = frame.array
             imagem.setflags(write=True)
 
-        facesDetectadas = detectorFace(imagem, 2)
-        for face in facesDetectadas :
-            e, t, d, b = (int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
-            pontosFaciais = detectorPontos(imagem, face)
-            descritorFacial = reconhecimentoFacial.compute_face_descriptor(imagem, pontosFaciais)
+            facesDetectadas = detectorFace(imagem, 2)
+            for face in facesDetectadas :
+                e, t, d, b = (int(face.left()), int(face.top()), int(face.right()), int(face.bottom()))
+                pontosFaciais = detectorPontos(imagem, face)
+                descritorFacial = reconhecimentoFacial.compute_face_descriptor(imagem, pontosFaciais)
 
-            listaDescritorFacial = [fd for fd in descritorFacial]
-            npArrayDescritorFacial = np.asarray(listaDescritorFacial, dtype=np.float64)
-            npArrayDescritorFacial = npArrayDescritorFacial[np.newaxis, :]
+                listaDescritorFacial = [fd for fd in descritorFacial]
+                npArrayDescritorFacial = np.asarray(listaDescritorFacial, dtype=np.float64)
+                npArrayDescritorFacial = npArrayDescritorFacial[np.newaxis, :]
 
-            distancias = np.linalg.norm(npArrayDescritorFacial - descritoresFaciais, axis=1)
-            minimo = np.argmin(distancias)
-            distanciaMinima = distancias[minimo]
+                distancias = np.linalg.norm(npArrayDescritorFacial - descritoresFaciais, axis=1)
+                minimo = np.argmin(distancias)
+                distanciaMinima = distancias[minimo]
 
-            if distanciaMinima <= limiar :
-                nome = os.path.split(indices[minimo])[1].split(".")[0]
-                userId = nome
-                logger.log('Usuário detectado')
-            else :
-                nome = "unknown"
+                if distanciaMinima <= limiar :
+                    nome = os.path.split(indices[minimo])[1].split(".")[0]
+                    userId = nome
+                    logger.log('Usuário detectado')
+                else :
+                    nome = "unknown"
 
-        #todo - Caso de exceção onde não foi possível encontrar nenhum user cadastrado
-        if cv2.waitKey(1) == ord('q'):
-            break
+            #todo - Caso de exceção onde não foi possível encontrar nenhum user cadastrado
+            if cv2.waitKey(1) == ord('q'):
+                break
+        rawCapture.truncate(0)
 
     cv2.destroyAllWindows()
 
