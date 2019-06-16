d5afe969887e569334307f4dec0ff67e775c749c
brunofornazari <bruno_fornazari@hotmail.com>
2019-06-16 17:26:01 -0300

    Teste de PIR

---
+++ testePIR.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+pir_sensor = 11 #GPIO pin 21
+
+GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
+
+current_state = 0
+
+while True:
+    try:
+        time.sleep(0.1)
+        current_state = GPIO.input(pir_sensor)
+        if current_state == 1:
+          print("GPIO pin %s is %s" % (pir_sensor, current_state)) # motion detected
+          print("LED is ON")
+          time.sleep(2)
+          print("LED is OFF")
+          time.sleep(4)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
