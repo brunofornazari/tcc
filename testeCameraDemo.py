675a49f7de36a8d6cfbf2dd4a5955800df417dce
Bruno Fornazari <bruno_fornazari@hotmail.com>
2019-06-21 18:29:17 -0300

    teste sem usar a video port

--- testeCameraDemo.py
+++ testeCameraDemo.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from imutils.video.pivideostream import PiVideoStream
+from testeCamera import PiVideoStream
 from imutils.video import FPS
 from picamera.array import PiRGBArray
 from picamera import PiCamera
