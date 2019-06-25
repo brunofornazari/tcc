4d1a169e0a3b38324884cb9ce70611931b3df34a
Bruno Fornazari <bruno_fornazari@hotmail.com>
2019-06-25 00:09:48 -0300

    criação do frone end do sistema de captura de novos usuários

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import time
 import utils.libs.messageHUB as messageHUB
 
 import server
-import app
+#import app
 
 if __name__ == '__main__' :
-    threadMain = threading.Thread(target=app.main)
+    #threadMain = threading.Thread(target=app.main)
     threadServer = threading.Thread(target=server.startServer)
 
     threadServer.start()
     time.sleep(3)
-    threadMain.start()
+    #threadMain.start()
