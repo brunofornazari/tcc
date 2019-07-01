cd8bb788a0451d410b6ad73df9c078080d1de7fc
Felipe Arias <felipe.arias@outlook.com.br>
2019-07-01 14:09:56 -0300

    Update deteccao_captura.py

--- facialRecognition/deteccao_captura.py
+++ facialRecognition/deteccao_captura.py
@@ -1,3 +1,17 @@
+***
+deteccao_captura.py
+
+Detecção_Captura.py é responsável por fazer a detecção e identificação a face através das caracteristicas capturadas nas imagens 
+retiradas pelo faceCapture.py. Para isto, ele segue uma rota de fluxo:
+    - Inicia o processo de detecção de face nas imagens capturadas;
+        -- Ele realiza até 10 tentativas de identificação, caso não consigua ele toda duas possíveis ações:
+            --- Caso não tenha identificado uma face, ele finaliza o processo;
+            --- Caso identifique mais de uma face, ele finaliza o processo;
+    - Realiza a comparação de caracteristiscas com usuários cadatrados;
+        -- Caso identifique, ele retorna informando a qual perfil pertence a face;
+        -- Caso não identifique as caracteristicas em nenhum dos perfils, ele retorna informando que é o perfil de um visitante;
+***
+
 import os
 import dlib
 import cv2
@@ -12,6 +26,7 @@ def main() :
 
 
 def getUserFromCamera() :
+    # Inicia a detecção
     detectorFace = dlib.get_frontal_face_detector()
     detectorPontos = dlib.shape_predictor(os.path.abspath("resources/shape_predictor_68_face_landmarks.dat"))
     reconhecimentoFacial = dlib.face_recognition_model_v1("resources/dlib_face_recognition_resnet_model_v1.dat")
@@ -24,13 +39,16 @@ def getUserFromCamera() :
 
     logger.log('Detectando usuário...')
     tentativas_conexao = 0
+    # Estabelece a quantia de tentativas
     while userId == -1 or tentativas_conexao < 10:
         imagem = cam.read()
         facesDetectadas = detectorFace(imagem, 2)
+        # Detecta as características através de pontos faciais específicos
         for face in facesDetectadas:
             pontosFaciais = detectorPontos(imagem, face)
 
             descritorFacial = reconhecimentoFacial.compute_face_descriptor(imagem, pontosFaciais)
+            # Faz a busca e comparação com perfís cadastrados
             if descritorFacial != None:
                 listaDescritorFacial = [fd for fd in descritorFacial]
                 npArrayDescritorFacial = np.asarray(listaDescritorFacial, dtype=np.float64)
@@ -41,6 +59,7 @@ def getUserFromCamera() :
                 distanciaMinima = distancias[minimo]
             else :
                 pass
+            # Retorna o perfil identificado
             if distanciaMinima <= limiar :
                 userId = os.path.split(indices[minimo])[1].split(".")[0]
             else :
@@ -54,7 +73,8 @@ def getUserFromCamera() :
             break
 
         tentativas_conexao += 1
-
+        
+        # Retorna o perfíl de visitante
     if userId == -1:
         userId = 'Visitante'
 
