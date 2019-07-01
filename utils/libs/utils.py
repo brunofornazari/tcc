9616ac41cb9237aad4aa36a7985dab93471faeac
Felipe Arias <felipe.arias@outlook.com.br>
2019-07-01 14:59:32 -0300

    Update utils.py

--- utils/libs/utils.py
+++ utils/libs/utils.py
@@ -1,3 +1,10 @@
+"""
+utils.py
+
+Utils.py é responsável por fazer a verificação de atributos de um objetos. Ele realiza uma operação booleana para identificar se
+o objeto retornado pertence ao devido processo em que participa.
+"""
+
 def check_attribute(object, property):
     for attribute in object:
         if attribute == property: return True
