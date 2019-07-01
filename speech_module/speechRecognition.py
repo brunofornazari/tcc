57990d68ca196b4da7791faab717d67cfe5497d3
Felipe Arias <felipe.arias@outlook.com.br>
2019-07-01 16:17:00 -0300

    Update speechRecognition.py

--- speech_module/speechRecognition.py
+++ speech_module/speechRecognition.py
@@ -6,6 +6,19 @@ frase e envio para a Wit.ai processar este aúdio e retornar com o valor requisi
     - Primeiro ele ajusta os dados que forem capturados através de uma instancia para reconhecimento de ruídos ambiente;
     - Depois disso, ele escuta as frases ditas e absorvidas pelo microfone;
     - Ele envia para o Wit.ai para transformação em texto;
+    - Recebe a intent do Wit.ai como um objeto;
+    - Extrai o metadado e valor do objeto para validar se a resposta recebida é valida:
+    - Confere se a confiabiliade do objeto é maior que 80%;
+        -- Caso não seja valido ou maior o que 80%, ele retorna pro usuário repetir a frase;
+    - Envia o argumento de entrada referente ao comando solicitado.
+Para seguir está rota, ele utiliza de alguns comandos que ditam quais serão os próximos processos. Sendo eles:
+    - Start_command: para iniciar o fluxo de aceitar um comando
+    - Stop_command: para interromper o fluxo de esperar por um comando
+    - Noticias_esportes: frases que sejam pertinentes para o retorno de noticias de esportes
+    - Noticias_politica: frases que sejam pertinentes para o retorno de noticias relacionada a política
+    - Noticias_economia: frases que sejam pertinentes para o retorno de noticias de economia
+    - Noticias_gerais: frases que sejam pertinentes para o retorno de noticias em geral
+    - Previsão_tempo: frases que sejam pertinentes a questões sobre a previsão meteorológica
 """
 
 import speech_recognition as sr
@@ -13,11 +26,14 @@ import utils.libs.logger as logger
 import utils.config.constants as constants
 import utils.libs.utils as util
 
+# Inicia as funções importadas a biblioteca speech_recognition
 def startRecognizing():
     r = sr.Recognizer()
     mic = sr.Microphone()
 
+    # Inicia a captura de comandos de voz com o microfone
     try:
+        # Envia para a Wit.ai a fala capturada
         with mic as source:
             r.adjust_for_ambient_noise(source)
             audio = r.listen(source)
@@ -34,9 +50,10 @@ def startRecognizing():
         logger.logError('Algo saiu errado')
         return 2
 
-
+# Recebe o objeto
 def get_intention(response):
 
+    # Valida se o objeto é valido
     if response is not None and util.check_attribute(response, 'entities') and util.check_attribute(response['entities'], 'intent'):
         intents = response['entities']['intent']
         for intent in intents:
@@ -45,8 +62,10 @@ def get_intention(response):
         return '', 0
 
 
+    # Extrai o metadado e valor do objeto
 def get_values(response):
     result = {}
+    # Realiza o teste de confiabilidade
     if response is not None and isinstance(response, dict) and response['entities'] and response['entities']:
         intents = response['entities']
 
@@ -56,6 +75,7 @@ def get_values(response):
     return result
 
 
+# Inicia o fluxo e reconhecimento dos comandos através da fala
 def wait_command(keep_execution, callback):
 
     while keep_execution:
